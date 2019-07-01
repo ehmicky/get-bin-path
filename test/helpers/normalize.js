012351db8b8ee3045c4569e9acd483023946681b
ehmicky <ehmicky@gmail.com>
2019-07-01 10:00:00 +0200

    Fix tests on Windows

--- test/helpers/normalize.js
+++ test/helpers/normalize.js
@@ -1,4 +1,4 @@
-import { relative } from 'path'
+import { posix } from 'path'
 
 import pkgDir from 'pkg-dir'
 
@@ -10,5 +10,5 @@ export const normalizeBinPath = async function(binPath) {
   }
 
   const rootDir = await ROOT_DIR
-  return relative(rootDir, binPath)
+  return posix.relative(rootDir, binPath)
 }
