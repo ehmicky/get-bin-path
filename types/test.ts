57190c8e708a328d1fd8f0e5a41ddf4eae421478
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-03-29 12:16:37 +0545

    Add typing test

---
+++ types/test.ts
@@ -0,0 +1,9 @@
+import { getBinPath, getBinPathSync } from 'get-bin-path';
+
+// Synchronous
+const binPath: string | undefined = getBinPathSync();
+
+// Asynchronous
+(async () => {
+  const binPath: string | undefined = await getBinPath();
+})();
