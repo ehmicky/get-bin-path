aa4e38f02369f2d33345b4cf3e0351fe649ba065
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-05-17 22:35:30 +0545

    Update doc blocks

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,7 +2,18 @@
  * Options for the getBinPath() function.
  */
 export interface BinaryOptions {
+  /**
+   * Name of the binary. Only needs to be specified when the package exports several binaries.
+   *
+   * Default: `package.json` `name` property
+   */
   name?: string;
+
+  /**
+   * Override the current directory, which is used when retrieving the `package.json`.
+   *
+   * Default: Current directory
+   */
   cwd?: string;
 }
 
@@ -22,7 +33,7 @@ export function getBinPath(options?: BinaryOptions): Promise<string | undefined>
  * Get the current package's binary path synchronously.
  *
  * @param {BinaryOptions} [options={}]        Additional options for the binary.
- * @returns {(Promise<string | undefined>)}
+ * @returns {string | undefined}
  *  Returns the binary absolute path and returns `undefined` if it could not be found.
  *
  * @example
