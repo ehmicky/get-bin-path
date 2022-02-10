7ca198d633f3e905140323cc0ea524206c068195
ehmicky <ehmicky@gmail.com>
2022-02-10 21:24:39 +0100

    Fix Ava config

--- ava.config.js
+++ ava.config.js
@@ -1,7 +1,1 @@
-import avaConfig from '@ehmicky/dev-tasks/ava.config.js'
-
-export default {
-  ...avaConfig,
-  // We need to use `process.chdir()` in tests
-  workerThreads: false,
-}
+export { default } from '@ehmicky/dev-tasks/ava.config.js'
