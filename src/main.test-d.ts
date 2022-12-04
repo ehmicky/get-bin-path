848d3a6a375dc9f40bfcfbd9cab62438525950e2
ehmicky <ehmicky@gmail.com>
2022-12-04 19:34:15 +0100

    Update type tests

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,9 +1,4 @@
-import {
-  expectError,
-  expectAssignable,
-  expectNotAssignable,
-  expectType,
-} from 'tsd'
+import { expectAssignable, expectNotAssignable, expectType } from 'tsd'
 
 import { getBinPath, getBinPathSync, BinaryOptions } from 'get-bin-path'
 
@@ -14,20 +9,28 @@ expectAssignable<BinaryOptions>({})
 await getBinPath({})
 getBinPathSync({})
 expectNotAssignable<BinaryOptions>(true)
-expectError(await getBinPath(true))
-expectError(getBinPathSync(true))
+// @ts-expect-error
+await getBinPath(true)
+// @ts-expect-error
+getBinPathSync(true)
 expectNotAssignable<BinaryOptions>({ unknown: true })
-expectError(await getBinPath({ unknown: true }))
-expectError(getBinPathSync({ unknown: true }))
+// @ts-expect-error
+await getBinPath({ unknown: true })
+// @ts-expect-error
+getBinPathSync({ unknown: true })
 
 expectAssignable<BinaryOptions>({ cwd: '/' })
 getBinPathSync({ cwd: '/' })
 await getBinPath({ cwd: '/' })
-expectError(await getBinPath({ cwd: true }))
-expectError(getBinPathSync({ cwd: true }))
+// @ts-expect-error
+await getBinPath({ cwd: true })
+// @ts-expect-error
+getBinPathSync({ cwd: true })
 
 expectAssignable<BinaryOptions>({ name: 'name' })
 getBinPathSync({ name: 'name' })
 await getBinPath({ name: 'name' })
-expectError(await getBinPath({ name: true }))
-expectError(getBinPathSync({ name: true }))
+// @ts-expect-error
+await getBinPath({ name: true })
+// @ts-expect-error
+getBinPathSync({ name: true })
